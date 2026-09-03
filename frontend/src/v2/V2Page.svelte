bdc1a67dc7dd542e698ee29fedaab95da8ddfb42
Desapoint <jasond1996@hotmail.com>
2026-09-02 23:26:35 -0400

    feat: render V2 demo shell

---
+++ frontend/src/v2/V2Page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import demoHtml from './demo.html?raw';
+</script>
+
+<svelte:head>
+  <title>Immich Companion V2</title>
+</svelte:head>
+
+<iframe class="v2-demo" title="Immich Companion V2 demo shell" srcdoc={demoHtml}></iframe>
+
+<style>
+  .v2-demo {
+    display: block;
+    width: 100vw;
+    height: 100vh;
+    border: 0;
+    background: #0b1016;
+  }
+</style>
