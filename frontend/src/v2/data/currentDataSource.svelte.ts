505492e3e1e028f269cebe65c1e8c50c3c5358ea
Desapoint <jasond1996@hotmail.com>
2026-09-06 14:40:37 -0400

    Compose demo saved search repository

--- frontend/src/v2/data/currentDataSource.svelte.ts
+++ frontend/src/v2/data/currentDataSource.svelte.ts
@@ -1,6 +1,7 @@
 import { createDemoLibraryDataSource } from './demo/demoLibraryDataSource.svelte';
 import { withDemoMediaProfiles } from './demo/demoMediaProfile';
+import { withDemoSavedSearches } from './demo/demoSavedSearches';
 
 // Single composition point for the V2 UI. Replacing this with an API-backed
 // implementation should not require page/component changes.
-export const libraryData = withDemoMediaProfiles(createDemoLibraryDataSource());
+export const libraryData = withDemoSavedSearches(withDemoMediaProfiles(createDemoLibraryDataSource()));
