1acdf22695824804e87f49e1a23f3f9f0bdbdc18
Desapoint <jasond1996@hotmail.com>
2026-09-06 13:18:59 -0400

    Apply demo media format profiles

--- frontend/src/v2/data/currentDataSource.svelte.ts
+++ frontend/src/v2/data/currentDataSource.svelte.ts
@@ -1,5 +1,6 @@
 import { createDemoLibraryDataSource } from './demo/demoLibraryDataSource.svelte';
+import { withDemoMediaProfiles } from './demo/demoMediaProfile';
 
 // Single composition point for the V2 UI. Replacing this with an API-backed
 // implementation should not require page/component changes.
-export const libraryData = createDemoLibraryDataSource();
+export const libraryData = withDemoMediaProfiles(createDemoLibraryDataSource());
