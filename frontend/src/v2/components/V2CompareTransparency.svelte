9a2176cd7cb5b228bfd3133be0f0725542b38d50
Desapoint <jasond1996@hotmail.com>
2026-09-06 19:16:52 -0400

    Track both transparency comparison image sizes

--- frontend/src/v2/components/V2CompareTransparency.svelte
+++ frontend/src/v2/components/V2CompareTransparency.svelte
@@ -9,6 +9,7 @@
     transform,
     opacity = $bindable(50),
     onselectedload,
+    onreferenceload,
     onviewport,
   }: {
     selectedSrc: string;
@@ -18,6 +19,7 @@
     transform: string;
     opacity?: number;
     onselectedload?: (event: Event) => void;
+    onreferenceload?: (event: Event) => void;
     onviewport?: (node: HTMLElement | null) => void;
   } = $props();
 
@@ -52,7 +54,7 @@
   onfocusin={showControls}
   onfocusout={hideControlsSoon}
 >
-  <div class="v2-compare-layer"><div class="v2-compare-transform" style={`transform:${transform}`}><img src={referenceSrc} alt={referenceLabel}></div></div>
+  <div class="v2-compare-layer"><div class="v2-compare-transform" style={`transform:${transform}`}><img src={referenceSrc} alt={referenceLabel} onload={onreferenceload}></div></div>
   <div class="v2-compare-layer top" style={`opacity:${opacity / 100}`}><div class="v2-compare-transform" style={`transform:${transform}`}><img src={selectedSrc} alt={selectedLabel} onload={onselectedload}></div></div>
   <div class="v2-compare-floating-controls v2-compare-hover">
     <V2RangeSlider label="Transparency" min={0} max={100} bind:value={opacity} suffix="%" track="fill" width={160} ariaLabel="Overlay transparency" />
