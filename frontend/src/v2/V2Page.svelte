a7abd6fa09ee1c9d8a7a6c95ab3f0bc342860ef5
desapoint <desapoint@outlook.com>
2026-09-07 19:12:12 -0400

    fix: mount live V2 tags page

--- frontend/src/v2/V2Page.svelte
+++ frontend/src/v2/V2Page.svelte
@@ -4,6 +4,7 @@
   import V2AlbumsPage from './pages/V2AlbumsPage.svelte';
   import V2SettingsPage from './pages/V2SettingsPage.svelte';
   import V2StatusPage from './pages/V2StatusPage.svelte';
+  import V2TagsPage from './pages/V2TagsPage.svelte';
   import {
     v2PageFromLegacyHash,
     v2PageFromPath,
@@ -71,6 +72,8 @@
     <V2StatusPage />
   {:else if activeKey === 'albums'}
     <V2AlbumsPage />
+  {:else if activeKey === 'tags'}
+    <V2TagsPage />
   {:else if activeKey === 'settings'}
     <V2SettingsPage />
   {:else}
